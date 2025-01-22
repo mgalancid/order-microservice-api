710da7bf7d7ab7fa4b5d6c8cfb3772793e4a40e8
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-22 12:19:19 -0300

    Implemented OrderService methods

--- order-service/src/main/java/com/mindhub/order_service/services/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/mindhub/order_service/services/impl/OrderServiceImpl.java
@@ -1,4 +1,58 @@
 package com.mindhub.order_service.services.impl;
 
-public class OrderServiceImpl {
+import com.mindhub.order_service.dtos.NewOrderEntityDTO;
+import com.mindhub.order_service.dtos.OrderEntityDTO;
+import com.mindhub.order_service.models.OrderEntity;
+import com.mindhub.order_service.models.OrderStatus;
+import com.mindhub.order_service.repositories.OrderItemRepository;
+import com.mindhub.order_service.repositories.OrderRepository;
+import com.mindhub.order_service.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private  OrderRepository orderRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Override
+    public OrderEntityDTO createOrder(NewOrderEntityDTO newOrderEntityDTO) {
+        OrderEntity order = new OrderEntity();
+        order.setUserId(newOrderEntityDTO.getUserId());
+        order.setStatus(newOrderEntityDTO.getStatus());
+
+        OrderEntity savedOrder = orderRepository.save(order);
+
+        return new OrderEntityDTO(savedOrder);
+    }
+
+    @Override
+    public List<OrderEntityDTO> getAllOrders() {
+        return orderRepository.findAll().stream()
+                .map(OrderEntityDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public OrderEntityDTO updateOrderStatus(Long orderId, OrderStatus status) {
+        OrderEntity order = orderRepository.findById(orderId)
+                .orElse(null);
+
+        if(order == null){
+            return null;
+        }
+
+        order.setStatus(status);
+        OrderEntity updatedOrder = orderRepository.save(order);
+
+        return new OrderEntityDTO(updatedOrder);
+    }
 }
+
