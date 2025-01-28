515016fcd1af9f4b61219dd0a9d127c604d78004
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-28 11:03:01 -0300

    Modified @LoadBalancer

--- order-service/src/main/java/com/mindhub/order_service/utils/RestTemplateConfig.java
+++ order-service/src/main/java/com/mindhub/order_service/utils/RestTemplateConfig.java
@@ -1,6 +1,7 @@
 package com.mindhub.order_service.utils;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder
                 .additionalMessageConverters(new MappingJackson2HttpMessageConverter())
